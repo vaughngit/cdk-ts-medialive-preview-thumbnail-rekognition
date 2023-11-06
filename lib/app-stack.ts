47c140f3d87fdbb3d086ec976b0bb2af8755d488
awsalvin <awsalvin@amazon.com>
2023-11-06 13:33:38 -0600

    updated license

--- lib/app-stack.ts
+++ lib/app-stack.ts
@@ -1,15 +1,5 @@
-/* Sample code, software libraries, command line tools, proofs of concept, templates, 
-or other related technology are provided as AWS Content or Third-Party Content under 
-the AWS Customer Agreement, or the relevant written agreement between you and AWS (whichever applies). 
-You should not use this AWS Content or Third-Party Content in your production accounts, 
-or on production or other critical data. You are responsible for testing, securing, and 
-optimizing the AWS Content or Third-Party Content, such as sample code, as appropriate for 
-production grade use based on your specific quality control practices and standards. 
-Deploying AWS Content or Third-Party Content may incur AWS charges for creating or 
-using AWS chargeable resources, such as running Amazon EC2 instances or using Amazon S3 storage.*/
-
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: MIT-0
+/* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0 */
 
 import * as cdk from 'aws-cdk-lib';
 import { CfnOutput, CfnParameter } from 'aws-cdk-lib';
